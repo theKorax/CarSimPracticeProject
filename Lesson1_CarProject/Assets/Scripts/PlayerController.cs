bea92d5a870a36094776b072ef9e3364c2074f8b
theKorax <zikoramadin@gmail.com>
2023-09-04 11:40:58 +0100

    Update PlayerController.cs

--- Lesson1_CarProject/Assets/Scripts/PlayerController.cs
+++ Lesson1_CarProject/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +17,6 @@ public class PlayerController : MonoBehaviour
     {
         // Move vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
     }
 }
